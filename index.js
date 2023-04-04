275180e342c7aef5bb09f4928fafd22c8ddacacf
aoshea <aoshea@users.noreply.github.com>
2023-04-04 13:43:20 +0100

    2 files modified

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ console.log(`Hello Node.js v${process.versions.node}!`);
 
 module.exports = {
   create_tiles,
+  order_char_set,
 };
 
 function create_tiles(wordset) {
   return wordset.split('');
+}
+
+function order_char_set(char_set) {
+  const char_list = char_set.split('|');
+  const ordered = char_list.slice(0, 1);
+  for (let i = 1; i < char_list.length; ++i) {
+    const x = char_list[i];
+    let prev = ordered[i - 1];
+    const chars = x.split('');
+    for (let j = 0; j < chars.length; ++j) {
+      const ch = chars[j];
+      if (prev.indexOf(ch) === -1) {
+        prev = prev + ch;
+      }
+    }
+    ordered.push(prev);
+  }
+  return ordered;
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,12 @@
 const test = require('tape');
-const { create_tiles } = require('./index');
+const { create_tiles, order_char_set } = require('./index');
+
+test('should sort word char set into ordered list', (t) => {
+  t.plan(1);
+  const char_set = 'aeg|aegr|aegrs|adegrs|abdegrs|abdegirs';
+  const expected = ['aeg', 'aegr', 'aegrs', 'aegrsd', 'aegrsdb', 'aegrsdbi'];
+  t.deepEqual(order_char_set(char_set), expected);
+});
 
 test('should create tile objects for each char in puzzle word', (t) => {
   t.plan(1);
